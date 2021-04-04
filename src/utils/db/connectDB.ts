e261037df407fd89519e8158ffff33c971bb7b40
ohexus <moskalenko.taras.d@gmail.com>
2021-04-04 13:28:51 +0300

    added connectDB util

---
+++ src/utils/db/connectDB.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+import logger from '../logger';
+
+import { LOGS } from '../../constants';
+
+const MONGO_URL: string | undefined = process.env.MONGO_URL;
+
+if (process.env.NODE_ENV === 'development') {
+  mongoose.set('debug', true);
+}
+
+export default async function connectDB(): Promise<typeof mongoose> {
+  if (!MONGO_URL) {
+    throw new Error('MONGO_URL is not found');
+  }
+
+  const connection = await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
+
+  logger.info(LOGS.SUCCESS.DB.CONNECTION);
+
+  return connection;
+}
