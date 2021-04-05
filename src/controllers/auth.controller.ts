bbbfe6a02bf4184f6cde9eb6836f06c550907895
ohexus <moskalenko.taras.d@gmail.com>
2021-04-05 20:38:12 +0300

    added AuthController

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from 'express';
+
+import { LOGS, STATUSES } from '@constants';
+import { errorResponse, successResponse } from '@utils/http';
+import { AuthService } from '@services';
+
+import { UserLogInRequest, UserSignUpRequest } from '@interfaces/http';
+
+class AuthController {
+  postSignUp = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const { email, username, password } = req.body as UserSignUpRequest;
+
+      const token = await AuthService.signUp({ email, username, password });
+
+      return successResponse(res, {
+        message: LOGS.SUCCESS.AUTH.SIGNUP,
+        payload: { jwtToken: token },
+        status: STATUSES.RESPONSE.SUCCESS.DEFAULT,
+      });
+    } catch (error) {
+      const { message } = error as Error;
+
+      return errorResponse(res, error, { message, status: STATUSES.RESPONSE.ERROR.UNAUTHORIZED });
+    }
+  };
+
+  postLogin = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const { login, password } = req.body as UserLogInRequest;
+
+      const token = await AuthService.logIn({ login, password });
+
+      return successResponse(res, {
+        message: LOGS.SUCCESS.AUTH.LOGIN,
+        payload: { jwtToken: token },
+        status: STATUSES.RESPONSE.SUCCESS.DEFAULT,
+      });
+    } catch (error) {
+      const { message } = error as Error;
+
+      return errorResponse(res, error, { message, status: STATUSES.RESPONSE.ERROR.UNAUTHORIZED });
+    }
+  };
+}
+
+export default new AuthController();
