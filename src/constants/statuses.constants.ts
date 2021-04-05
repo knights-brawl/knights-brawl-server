0ace8d96cfa62cc928e314ac206a46168c75bb41
ohexus <moskalenko.taras.d@gmail.com>
2021-04-05 20:29:48 +0300

    added LIMITS, LOGS and STATUSES consts

---
+++ src/constants/statuses.constants.ts
@@ -0,0 +1,13 @@
+const STATUSES = {
+  RESPONSE: {
+    SUCCESS: {
+      DEFAULT: 200,
+    },
+    ERROR: {
+      DEFAULT: 400,
+      UNAUTHORIZED: 401,
+    },
+  },
+};
+
+export default STATUSES;
