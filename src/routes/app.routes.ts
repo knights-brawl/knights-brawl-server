11281fc080dae1b9ecccefb6752efd847eb480c6
ohexus <moskalenko.taras.d@gmail.com>
2021-04-06 22:02:00 +0300

    added missing userRouter to appRouter

--- src/routes/app.routes.ts
+++ src/routes/app.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { default as authRouter } from './auth.routes';
+import { default as userRouter } from './user.routes';
 import { authMiddleware } from '@middlewares';
 
 const appRouter = Router();
@@ -8,5 +9,7 @@ const appRouter = Router();
 appRouter.use('/api/auth', authRouter);
 
 appRouter.use(authMiddleware);
+
+appRouter.use('/api/user', userRouter);
 
 export default appRouter;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,2 +1,3 @@
 export { default as appRouter } from './app.routes';
 export { default as authRouter } from './auth.routes';
+export { default as userRouter } from './user.routes';
