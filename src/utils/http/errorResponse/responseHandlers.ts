d5d3bb748346f6d98c4f72020c2b752b92eee05b
ohexus <moskalenko.taras.d@gmail.com>
2021-04-09 09:04:04 +0300

    removed successResponse

--- src/utils/http/errorResponse/responseHandlers.ts
+++ src/utils/http/errorResponse/responseHandlers.ts
@@ -5,32 +5,13 @@ import { logger } from '@utils';
 
 import { BackServerResponse } from '@interfaces/http';
 
-const defaultSuccessResponse: BackServerResponse = {
-  message: LOGS.SUCCESS.HTTP.DEFAULT,
-  payload: { status: LOGS.SUCCESS.HTTP.DEFAULT },
-  status: STATUSES.RESPONSE.SUCCESS.DEFAULT,
-};
-
 const defaultErrorResponse: BackServerResponse = {
   message: LOGS.ERROR.HTTP.DEFAULT,
   payload: { status: LOGS.SUCCESS.HTTP.DEFAULT },
   status: STATUSES.RESPONSE.ERROR.DEFAULT,
 };
 
-export function successResponse(
-  res: Response,
-  { message, payload, status }: BackServerResponse = defaultSuccessResponse,
-): Response {
-  logger.info({ message, payload, status });
-
-  return res.status(status).json({
-    message,
-    payload: payload || {},
-    status,
-  });
-}
-
-export function errorResponse(
+export default function errorResponse(
   res: Response,
   error: Error,
   { message, status }: BackServerResponse = defaultErrorResponse,
