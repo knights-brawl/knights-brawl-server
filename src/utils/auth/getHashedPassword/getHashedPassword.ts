429545d8f1a8bde1d85d11b589bf66df0e9d9e46
ohexus <moskalenko.taras.d@gmail.com>
2021-04-12 21:34:52 +0300

    moved destructuring of process.env inside getHashedPassword

--- src/utils/auth/getHashedPassword/getHashedPassword.ts
+++ src/utils/auth/getHashedPassword/getHashedPassword.ts
@@ -1,14 +1,14 @@
 import { scrypt } from 'crypto';
 
-const salt = process.env.SALT;
-
 export default function getHashedPassword(password: string): Promise<string> {
-  if (!salt) {
-    throw new Error('salt is not specified!');
+  const { SALT } = process.env;
+
+  if (!SALT) {
+    throw new Error('SALT is not specified!');
   }
 
   return new Promise((resolve) => {
-    scrypt(password, salt, 64, (err, hash) => {
+    scrypt(password, SALT, 64, (err, hash) => {
       if (err) {
         throw err;
       }
